245612761ad346e142ef0c2fe6ba9922d0319c42
Evillanuevaj <119708101+Evillanuevaj@users.noreply.github.com>
2023-02-10 11:07:27 -0500

    Add files via upload

---
+++ MicroserviceYankiPurse/src/main/java/com/bmicro/purse/purse/yankie/service/PurseYankieEventsService.java
@@ -0,0 +1,35 @@
+package com.bmicro.purse.purse.yankie.service;
+
+import com.bmicro.purse.purse.yankie.entity.PurseYankie;
+import com.bmicro.purse.purse.yankie.events.Event;
+import com.bmicro.purse.purse.yankie.events.EventType;
+import com.bmicro.purse.purse.yankie.events.PurseYankieCreatedEvent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Component
+public class PurseYankieEventsService {
+
+    @Autowired
+    private KafkaTemplate<String, Event<?>> producer;
+
+    @Value("${topic.purseYankie.name:purseYankies}")
+    private String topicPurseYankie;
+
+    public void publish(PurseYankie purseYankie) {
+
+        PurseYankieCreatedEvent created = new PurseYankieCreatedEvent();
+        created.setData(purseYankie);
+        created.setId(UUID.randomUUID().toString());
+        created.setType(EventType.CREATED);
+        created.setDate(new Date());
+
+        this.producer.send(topicPurseYankie, created);
+    }
+
+}
